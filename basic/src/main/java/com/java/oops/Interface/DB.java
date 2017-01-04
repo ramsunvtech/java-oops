b2ba48bfc5f50b143676d2b17037a561fcfb0a89
Venkatraman.R <vramamoorthy@paypalcorp.com>
2017-01-04 20:50:02 +0530

    Added Parent POM and Updated Basic Child POM with MVN compiler java version

--- basic/src/main/java/com/java/oops/Interface/DB.java
+++ basic/src/main/java/com/java/oops/Interface/DB.java
@@ -2,18 +2,13 @@ package com.java.oops.Interface;
 
 import java.util.Objects;
 
-/**
- * Created by vramamoorthy on 08/12/16.
- */
 public interface DB {
 
     Object connect();
     String chooseDB();
     String selectQuery();
 
-    default String employeeList() {
-        return "";
-    }
+    default String employeeList() { return ""; }
 
     default String staffList() {
         return "";
